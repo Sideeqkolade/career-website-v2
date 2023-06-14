32b9ebd7ecb40eb6f5b433e89ad3c7a9e8f21321
Sideeqkolade <93819623+Sideeqkolade@users.noreply.github.com>
2023-06-14 02:47:05 +0000

    added the flask project

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_World():
+  return "Hello, World!"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
